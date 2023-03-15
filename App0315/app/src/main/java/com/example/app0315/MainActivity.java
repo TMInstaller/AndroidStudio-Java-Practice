316a623c68228b33d924d2ab01e7874f4ee041bb
TMInstaller <ajvldmlqjqclr@naver.com>
2023-03-15 11:59:54 +0900

    Intent Practice

--- App0315/app/src/main/java/com/example/app0315/MainActivity.java
+++ App0315/app/src/main/java/com/example/app0315/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.app0315;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn3, btn4, btn5, btn6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
+        btn5 = findViewById(R.id.button5);
+        btn6 = findViewById(R.id.button6);
+
+        btn3.setBackgroundColor(Color.GRAY);
+        btn4.setBackgroundColor(Color.GREEN);
+        btn5.setBackgroundColor(Color.RED);
+        btn6.setBackgroundColor(Color.YELLOW);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+                startActivity(intent);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:/119"));
+                startActivity(intent);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images"));
+                startActivity(intent);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
