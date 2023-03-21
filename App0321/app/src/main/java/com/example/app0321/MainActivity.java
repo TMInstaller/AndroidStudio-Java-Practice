70c710180e8cb23cf3e2da3261483adb3e35b8e3
TMInstaller <ajvldmlqjqclr@naver.com>
2023-03-21 10:55:19 +0900

    Complete Activity2

--- App0321/app/src/main/java/com/example/app0321/MainActivity.java
+++ App0321/app/src/main/java/com/example/app0321/MainActivity.java
@@ -3,12 +3,109 @@ package com.example.app0321;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText et1, et2;
+    TextView tv;
+    Button btn1, btn2, btn3, btn4, btn5;
+    String num1, num2;
+    Double result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("초간단 계산기");
+        et1 = (EditText) findViewById(R.id.editText1);
+        et2 = (EditText) findViewById(R.id.editText2);
+        tv = (TextView) findViewById(R.id.textView);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = et1.getText().toString();
+                num2 = et2.getText().toString();
+                if(num1.trim().equals("") || num2.trim().equals("")){
+                    Toast.makeText(getApplicationContext(), "숫자를 입력 하세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    result = Double.parseDouble(num1) + Double.parseDouble(num2);
+                    tv.setText("계산 결과 : " + result.toString());
+                }
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = et1.getText().toString();
+                num2 = et2.getText().toString();
+                if(num1.trim().equals("") || num2.trim().equals("")){
+                    Toast.makeText(getApplicationContext(), "숫자를 입력 하세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    result = Double.parseDouble(num1) - Double.parseDouble(num2);
+                    tv.setText("계산 결과 : " + result.toString());
+                }
+
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = et1.getText().toString();
+                num2 = et2.getText().toString();
+                if(num1.trim().equals("") || num2.trim().equals("")){
+                    Toast.makeText(getApplicationContext(), "숫자를 입력 하세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    result = Double.parseDouble(num1) * Double.parseDouble(num2);
+                    tv.setText("계산 결과 : " + result.toString());
+                }
+
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = et1.getText().toString();
+                num2 = et2.getText().toString();
+                if(num1.trim().equals("") || num2.trim().equals("")){
+                    Toast.makeText(getApplicationContext(), "숫자를 입력 하세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    if(num2.trim().equals("0")){
+                        Toast.makeText(getApplicationContext(), "0으로 나눌 수 없습니다.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        result = Double.parseDouble(num1) / Double.parseDouble(num2);
+                        tv.setText("계산 결과 : " + result.toString());
+                    }
+                }
+
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = et1.getText().toString();
+                num2 = et2.getText().toString();
+                if(num1.trim().equals("") || num2.trim().equals("")){
+                    Toast.makeText(getApplicationContext(), "숫자를 입력 하세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    if(num2.trim().equals("0")){
+                        Toast.makeText(getApplicationContext(), "0으로 나머지를 구할 수 없습니다.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        result = Double.parseDouble(num1) % Double.parseDouble(num2);
+                        tv.setText("계산 결과 : " + result.toString());
+                    }
+                }
+
+            }
+        });
     }
 }
--- App0321/app/src/main/java/com/example/app0321/MainActivity2.java
+++ App0321/app/src/main/java/com/example/app0321/MainActivity2.java
@@ -3,12 +3,71 @@ package com.example.app0321;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    CheckBox cb1, cb2, cb3;
+    ImageView iv1, iv2, iv3;
+    Switch switch1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        cb1 = (CheckBox) findViewById(R.id.checkBox1);
+        cb2 = (CheckBox) findViewById(R.id.checkBox2);
+        cb3 = (CheckBox) findViewById(R.id.checkBox3);
+        iv1 = (ImageView) findViewById(R.id.imageView1);
+        iv2 = (ImageView) findViewById(R.id.imageView2);
+        iv3 = (ImageView) findViewById(R.id.imageView3);
+        switch1 = (Switch) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    cb1.setVisibility(View.VISIBLE);
+                    cb2.setVisibility(View.VISIBLE);
+                    cb3.setVisibility(View.VISIBLE);
+                } else {
+                    cb1.setVisibility(View.INVISIBLE);
+                    cb2.setVisibility(View.INVISIBLE);
+                    cb3.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+                    iv1.setVisibility(View.VISIBLE);
+                } else {
+                    iv1.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        cb2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+                    iv2.setVisibility(View.VISIBLE);
+                } else {
+                    iv2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        cb3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+                    iv3.setVisibility(View.VISIBLE);
+                } else {
+                    iv3.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
