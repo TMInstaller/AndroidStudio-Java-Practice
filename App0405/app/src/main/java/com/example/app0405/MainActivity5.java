f2bf2fe9b412151c99d92bf81ce365be6d58732d
TMInstaller <ajvldmlqjqclr@naver.com>
2023-04-05 12:36:58 +0900

    ViewFlipper

---
+++ App0405/app/src/main/java/com/example/app0405/MainActivity5.java
@@ -0,0 +1,35 @@
+package com.example.app0405;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    Button btnPrev, btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+        final ViewFlipper viewFlipper;
+        btnPrev = (Button) findViewById(R.id.button51);
+        btnNext = (Button) findViewById(R.id.button52);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper1);
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewFlipper.showPrevious();
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewFlipper.showNext();
+            }
+        });
+    }
+}
