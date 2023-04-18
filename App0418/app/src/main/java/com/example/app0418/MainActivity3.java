165049645891bea6cea9bcc218ef9ce3f5ed0204
TMInstaller <ajvldmlqjqclr@naver.com>
2023-04-18 10:11:51 +0900

    토스트 연습

--- App0418/app/src/main/java/com/example/app0418/MainActivity3.java
+++ App0418/app/src/main/java/com/example/app0418/MainActivity3.java
@@ -3,12 +3,30 @@ package com.example.app0418;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        btn = findViewById(R.id.button4);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast tm = Toast.makeText(getApplicationContext(), "토스트 연습", Toast.LENGTH_SHORT);
+                Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+                int offx = (int) (Math.random() * display.getWidth());
+                int offy = (int) (Math.random() * display.getHeight());
+                tm.setGravity(Gravity.TOP | Gravity.LEFT, offx, offy);
+                tm.show();
+            }
+        });
     }
 }
